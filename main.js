f4e6f0d07c4357e770d8b6aa35d4436d37d57183
LakshanyaPrabhu <88978072+Lakshanya-P@users.noreply.github.com>
2023-03-30 18:55:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -11,5 +11,35 @@ function gotResults(error,results){
     }
     else{
         console.log(results);
+        random_r=Math.floor(Math.random()*255)+1;
+        random_g=Math.floor(Math.random()*255)+1;
+        random_b=Math.floor(Math.random()*255)+1;
+
+        document.getElementById("result_label").innerHTML="I can hear -" + results[0].label;
+        document.getElementById("result_confidence").innerHTML="Accuracy - " + (results[0].confidence*100).toFixed(2) + "%";
+        document.getElementById("result_label").style.color="rgb("+random_r+","+random_g+","+random_b+")";
+        document.getElementById("result_confidence").style.color="rgb("+random_r+","+random_g+","+random_b+")";
+
+        img1=document.getElementById("alien1");
+
+
+        if(results[0].label=="meow"){
+            img1.src="cat.gif";
+        }
+        
+        else if(results[0].label=="Barking"){
+            img1.src="dog.gif";
+
+        }
+
+        else if(results[0].label=="roar"){
+            img1.src="lion.gif";
+    
+        }
+
+        else{
+            img1.src="ear.png";
+        }
+
     }
 }
